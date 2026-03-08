d2446c8302be45f737873a52d2da38db500e3773
Rusif Rakhmanov <rahmanovrusif@gmail.com>
2026-03-08 18:52:17 +0300

    test(participant): added new participant factory tests

--- tests/unit/entities/participant/test_participant_factory.py
+++ tests/unit/entities/participant/test_participant_factory.py
@@ -1,13 +1,16 @@
+import pytest
 from hypothesis import HealthCheck, given, settings
+from hypothesis import strategies as st
 
 from dreamteams.entities.common.clock import Clock
+from dreamteams.entities.errors.participant import InvalidParticipantDataError
 from dreamteams.entities.participant.entity import (
     Participant,
     ParticipantData,
     participant_factory,
 )
 from dreamteams.entities.user import User
-from tests.unit.composite import valid_participant_data
+from tests.unit.composite import participant_contact_data, valid_participant_data
 
 
 @settings(suppress_health_check=[HealthCheck.function_scoped_fixture])
@@ -33,3 +36,78 @@ def test_create_participant_with_valid_data(
         created_at=participant.created_at,
         updated_at=participant.updated_at,
     )
+
+
+@settings(suppress_health_check=[HealthCheck.function_scoped_fixture])
+@given(valid_participant_data(), st.data())
+def test_participant_contacts_are_unique(
+    user_without_organizer: User,
+    clock: Clock,
+    participant_data: ParticipantData,
+    data: st.DataObject,
+) -> None:
+    """Test cannot create competition with duplicate contacts."""
+    participant_data.contacts = [data.draw(participant_contact_data())] * 2
+
+    with pytest.raises(InvalidParticipantDataError, match="Contact URLs must be unique"):
+        participant_factory(
+            data=participant_data,
+            user=user_without_organizer,
+            clock=clock,
+        )
+
+
+@pytest.mark.parametrize("empty_string", ["", " ", "   "])
+@settings(suppress_health_check=[HealthCheck.function_scoped_fixture])
+@given(valid_participant_data())
+def test_paticipant_full_name_is_not_empty(
+    empty_string: str,
+    user_without_organizer: User,
+    clock: Clock,
+    data: ParticipantData,
+) -> None:
+    """Test cannot create participant with empty full name."""
+    data.full_name = empty_string
+
+    with pytest.raises(InvalidParticipantDataError, match="Full name must not be empty"):
+        participant_factory(
+            data=data,
+            user=user_without_organizer,
+            clock=clock,
+        )
+
+
+@settings(suppress_health_check=[HealthCheck.function_scoped_fixture])
+@given(valid_participant_data())
+def test_participant_skills_is_not_empty(
+    user_without_organizer: User,
+    clock: Clock,
+    data: ParticipantData,
+) -> None:
+    """Test cannot create participant with empty skills."""
+    data.skills = []
+
+    with pytest.raises(InvalidParticipantDataError, match="Skills list must not be empty"):
+        participant_factory(
+            data=data,
+            user=user_without_organizer,
+            clock=clock,
+        )
+
+
+@settings(suppress_health_check=[HealthCheck.function_scoped_fixture])
+@given(valid_participant_data())
+def test_participant_preferred_domains_is_not_empty(
+    user_without_organizer: User,
+    clock: Clock,
+    data: ParticipantData,
+) -> None:
+    """Test cannot create participant with empty preferred domains."""
+    data.preferred_domains = []
+
+    with pytest.raises(InvalidParticipantDataError, match="Preferred domains list not be empty"):
+        participant_factory(
+            data=data,
+            user=user_without_organizer,
+            clock=clock,
+        )
