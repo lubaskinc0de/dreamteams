7745463057b9ed553c1d5e7064d75a3e135c4912
lubaskin <lubaskincorporation@gmail.com>
2026-05-28 18:30:02 +0300

    chore: change uow

--- src/dreamteams_common/uow.py
+++ src/dreamteams_common/uow.py
@@ -9,19 +9,15 @@ class UoW(Protocol):
     @abstractmethod
     async def commit(self) -> None:
         """Commits all pending changes in the current transaction to the database."""
-        ...
 
     @abstractmethod
-    def add(self, instance: object) -> None:
+    def add(self, instance: Any) -> None:
         """Registers a new entity instance to be persisted when the transaction is committed."""
-        ...
 
     @abstractmethod
-    async def delete(self, instance: object) -> None:
+    async def delete(self, instance: Any) -> None:
         """Marks an entity instance for deletion when the transaction is committed."""
-        ...
 
     @abstractmethod
     async def flush(self, objects: Sequence[Any] | None = None) -> None:
         """Flushes pending changes to the database without committing the transaction."""
-        ...
